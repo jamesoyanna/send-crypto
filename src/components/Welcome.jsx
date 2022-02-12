fbff5371cb47f1a0dac917fce75401497c2a6fce
Kelvin <engrjayt200@gmail.com>
2022-02-12 19:42:44 +0100

    Add styling

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,7 +7,7 @@ import { TransactionContext } from "../context/TransactionContext";
 import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
-const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-black-400 text-sm font-black text-black";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
@@ -16,7 +16,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     step="0.0001"
     value={value}
     onChange={(e) => handleChange(e, name)}
-    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    className="my-2 w-full rounded-sm p-2 outline-none bg-black text-white border-none text-sm white-glassmorphism"
   />
 );
 
@@ -57,49 +57,49 @@ const Welcome = () => {
           )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+            <div className={`rounded-tl-2xl text-black ${companyCommonStyles}`}>
               Reliability
             </div>
             <div className={companyCommonStyles}>Security</div>
-            <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+            <div className={`sm:rounded-tr-2xl text-black ${companyCommonStyles}`}>
               Ethereum
             </div>
-            <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+            <div className={`sm:rounded-bl-2xl text-black ${companyCommonStyles}`}>
               Web 3.0
             </div>
             <div className={companyCommonStyles}>Low Fees</div>
-            <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+            <div className={`rounded-br-2xl text-black ${companyCommonStyles}`}>
               Blockchain
             </div>
           </div>
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card bg-[#2952e3]">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                  <SiEthereum fontSize={21} color="#fff" />
+                  <SiEthereum fontSize={21} color="#000" />
                 </div>
-                <BsInfoCircle fontSize={17} color="#fff" />
+                <BsInfoCircle fontSize={17} color="#000" />
               </div>
               <div>
-                <p className="text-white font-light text-sm">
+                <p className="text-black font-light text-sm">
                   {shortenAddress(currentAccount)}
                 </p>
-                <p className="text-white font-semibold text-lg mt-1">
+                <p className="text-black font-semibold text-lg mt-1">
                   Ethereum
                 </p>
               </div>
             </div>
           </div>
-          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center bg-[#000066] rounded-3xl">
             <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
             <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
-            <div className="h-[1px] w-full bg-gray-400 my-2" />
+            <div className="h-[1px] w-full bg-white-400 my-2" />
 
             {isLoading
               ? <Loader />
