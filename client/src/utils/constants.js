c3b9822729246922adabd59cbb0f2aae21049183
James <engrjayt200@gmail.com>
2022-02-27 21:50:16 +0100

    Add components

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x13aB2F07e394822bdfDac04235B3Eba2E717461E";
+//export const contractABI = "i3i3i3i33ii3";
+export const contractABI = abi.abi;
