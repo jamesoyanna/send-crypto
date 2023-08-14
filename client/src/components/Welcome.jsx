f5b89fb78a19a428ad006d38d2e0b328435394e3
jamesoyanna <engrjayt200@gmail.com>
2023-08-14 23:11:16 +0100

    Update text on Hero component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -38,10 +38,10 @@ const Welcome = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-4xl text-black text-gradient py-1">
-            Send Crypto <br /> across the world
+           Send cryptocurrency globally.
           </h1>
           <p className="text-left mt-5 text-black font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on Send Crypto.
+            Go on a journey through the realm of digital currency. Seamlessly transact cryptocurrencies on the Send Crypto platform.
           </p>
           {!currentAccount && (
             <button
